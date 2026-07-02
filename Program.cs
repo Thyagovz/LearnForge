3b882bde28cfb8573abf204c7b6b34a5c91e64e4
Thyago Euclides <flooddk@gmail.com>
2026-07-02 16:03:06 -0400

    Add CareerItem instances to demonstrate career structure; update Program to display career items

--- Program.cs
+++ Program.cs
@@ -17,5 +17,32 @@ class Program
             Console.WriteLine(article.Title);
             Console.WriteLine(article.Url);
         }
+
+        var courses = new List<Course>();
+        var courseOOP = new Course("Fundamentos OOP", "fundamentos-oop");
+        var courseCsharp = new Course("Fundamentos C#", "fundamentos-csharp");
+        var courseAspNet = new Course("Fundamentos ASP .NET", "fundamentos-aspnet");
+        courses.Add(courseOOP);
+        courses.Add(courseCsharp);
+        courses.Add(courseAspNet);
+
+        var careers = new List<Career>();
+        var careerDotnet = new Career("Especialista .NET", "expecialista-dotnet");
+        var careerItem2 = new CareerItem(2, "Aprenda OOP", "", null);
+        var careerItem = new CareerItem(1, "Comece por aqui", "", null);
+        var careerItem3 = new CareerItem(3, "Aprenda .NET", "", null);
+        careerDotnet.Items.Add(careerItem2);
+        careerDotnet.Items.Add(careerItem3);
+        careerDotnet.Items.Add(careerItem);
+        careers.Add(careerDotnet);
+
+        foreach (var career in careers)
+        {
+            Console.WriteLine(career.Title);
+            foreach (var item in career.Items.OrderBy(x=> x.Order))
+            {
+                Console.WriteLine($"{item.Order} - {item.Title}");
+            }
+        }
     }
 }
